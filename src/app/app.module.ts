d5a74ddb93229f96880f7c7912b2753581d8476d
loki_cheng <loki_cheng@wistronits.com>
2018-11-05 16:12:31 +0800

    add service modify src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { AppComponent } from './page/app/app.component';
+import { HeroesComponent } from './page/heroes/heroes.component';
+import { HeroDetailComponent } from './page/hero-detail/hero-detail.component';
+import { MessagesComponent } from './page/messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
