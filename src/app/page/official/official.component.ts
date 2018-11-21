b4913e81bbdd57cd952ca80bf8bbe74147e00689
fy0015 <359224568@qq.com>
2018-11-21 15:00:15 +0800

    adding annotations in officialPage

--- src/app/page/official/official.component.ts
+++ src/app/page/official/official.component.ts
@@ -7,18 +7,29 @@ import { levelData, mapDate } from 'src/app/service/data.';
   styleUrls: ['./official.component.scss']
 })
 export class OfficialComponent implements OnInit {
+  // 地图及等级所需经验信息
   mapInfos: {}[];
-  levelInfos: {}[];
+  levelInfos: any[];
 
+  /**
+   * 等级滑动选择条刻度标记
+   */
+  marks: {};
+
+  // 计算所需参数
   startLevel: number = 1;
   endLevel: number = 100;
   level: number[] = [1, 100];
-  marks: {};
-  map: {};
+  selectMap: {};
   isCaptain: boolean = false;
   isMvp: boolean = false;
   isVow: boolean = false;
   isHolyLiver: boolean = false;
+
+  /**
+   * 是否计算完成
+   */
+  isSure: boolean = false;
 
   constructor() {
     this.mapInfos = mapDate;
@@ -35,23 +46,36 @@ export class OfficialComponent implements OnInit {
     };
   }
 
-  initMapInfo(map): void {
-    this.map = map;
+  /**
+   *
+   * @param selectMap 选择地图
+   */
+  initMapInfo(selectMap): void {
+    this.selectMap = selectMap;
   }
 
+  /**
+   * 清空所选信息
+   */
   clearSelect(): void {
     // this.startLevel = 1;
     // this.endLevel = 100;
     // this.level = [1, 100];
-    this.map = null;
+    this.selectMap = null;
     this.isCaptain = false;
     this.isMvp = false;
     this.isVow = false;
     this.isHolyLiver = false;
+    this.isSure = false;
+    this.sumtest = null;
   }
 
+  /**
+   * 保持改变后等级排序
+   */
   changeLevel(): void {
     // console.log('changeStart', this.startLevel, this.endLevel);
+    this.sumtest = null;
     if (this.startLevel > this.endLevel) {
       let middle = this.startLevel;
       this.startLevel = this.endLevel;
@@ -63,13 +87,24 @@ export class OfficialComponent implements OnInit {
     this.level = [this.startLevel, this.endLevel];
   }
 
+  /**
+   * 等级滑动条数据改变后
+   */
   levelSliderChange(): void {
     this.startLevel = this.level[0];
     this.endLevel = this.level[1];
     this.changeLevel();
   }
 
+  sumtest: number;
+  /**
+   * 计算
+   */
   sure(): void {
-
+    this.sumtest = 0;
+    for (let i = this.startLevel; i < this.endLevel; i++) {
+      this.sumtest += this.levelInfos[i - 1];
+    }
+    this.isSure = true;
   }
 }
